37e689c9580b12a3a22a214c9367df1aaa574f88
Jaime Jorge <jaime.jorge@adevinta.com>
2026-02-24 23:18:04 +0100

    fix labels

--- scripts/kubeconfig.sh
+++ scripts/kubeconfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-[[ "${DEBUG}" == "true" ]] && set -x
+[[ "${DEBUG:-false}" == "true" ]] && set -x
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
